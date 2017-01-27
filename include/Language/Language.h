85101519f6d946a897f9d8587c1ba036032f7018
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2017-01-27 13:41:05 -0600

    * Initial Add

---
+++ include/Language/Language.h
@@ -0,0 +1,19 @@
+//
+//  Language.h
+//  Language
+//
+//  Created by Todd Ditchendorf on 27.01.17.
+//  Copyright © 2017 Celestial Teapot. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Language.
+FOUNDATION_EXPORT double LanguageVersionNumber;
+
+//! Project version string for Language.
+FOUNDATION_EXPORT const unsigned char LanguageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Language/PublicHeader.h>
+
+
