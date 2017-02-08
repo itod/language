8d9389d30fa289b627aea599900174a85fd2dfb9
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2017-02-07 18:14:21 -0600

    * test cu

--- test/XPRelationalExpressionTests.m
+++ test/XPRelationalExpressionTests.m
@@ -25,113 +25,53 @@
 }
 
 - (void)test1Lt1 {
-    NSString *input = @"var foo=1 < 1;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDFalse([[interp.globals objectForName:@"foo"] boolValue]);
+    [self eval:@"var foo=1 < 1;"];
+    TDFalse([self boolForName:@"foo"]);
 }
 
 - (void)test1LtEq1 {
-    NSString *input = @"var foo=1 <= 1;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDTrue([[interp.globals objectForName:@"foo"] boolValue]);
+    [self eval:@"var foo=1 <= 1;"];
+    TDTrue([self boolForName:@"foo"]);
 }
 
 - (void)test1Lt2 {
-    NSString *input = @"var foo=1 < 2;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDTrue([[interp.globals objectForName:@"foo"] boolValue]);
+    [self eval:@"var foo=1 < 2;"];
+    TDTrue([self boolForName:@"foo"]);
 }
 
 - (void)test1LtEq2 {
-    NSString *input = @"var foo=1 <= 2;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDTrue([[interp.globals objectForName:@"foo"] boolValue]);
+    [self eval:@"var foo=1 <= 2;"];
+    TDTrue([self boolForName:@"foo"]);
 }
 
 - (void)test1Gt1 {
-    NSString *input = @"var foo=1 > 1;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDFalse([[interp.globals objectForName:@"foo"] boolValue]);
+    [self eval:@"var foo=1 > 1;"];
+    TDFalse([self boolForName:@"foo"]);
 }
 
 - (void)test1GtEq1 {
-    NSString *input = @"var foo=1 >= 1;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDTrue([[interp.globals objectForName:@"foo"] boolValue]);
+    [self eval:@"var foo=1 >= 1;"];
+    TDTrue([self boolForName:@"foo"]);
 }
 
 - (void)test1Gt2 {
-    NSString *input = @"var foo=1 > 2;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDFalse([[interp.globals objectForName:@"foo"] boolValue]);
+    [self eval:@"var foo=1 > 2;"];
+    TDFalse([self boolForName:@"foo"]);
 }
 
 - (void)test1GtEq2 {
-    NSString *input = @"var foo=1 >= 2;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDFalse([[interp.globals objectForName:@"foo"] boolValue]);
+    [self eval:@"var foo=1 >= 2;"];
+    TDFalse([self boolForName:@"foo"]);
 }
 
 - (void)testNeg0Lt0 {
-    NSString *input = @"var foo=-0 < 0;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDFalse([[interp.globals objectForName:@"foo"] boolValue]);
+    [self eval:@"var foo=-0 < 0;"];
+    TDFalse([self boolForName:@"foo"]);
 }
 
 - (void)testNeg0Gt0 {
-    NSString *input = @"var foo=-0.0 > 0.0;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDFalse([[interp.globals objectForName:@"foo"] boolValue]);
+    [self eval:@"var foo=-0.0 > 0.0;"];
+    TDFalse([self boolForName:@"foo"]);
 }
 
 @end
