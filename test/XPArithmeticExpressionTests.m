a79d604e0852c0036d0cabb2be119c2e6c90f7df
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2017-02-07 18:15:13 -0600

    * test cu

--- test/XPArithmeticExpressionTests.m
+++ test/XPArithmeticExpressionTests.m
@@ -24,267 +24,123 @@
 }
 
 - (void)test1SpacePlusSpace2 {
-    NSString *input = @"var foo=1 + 2;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDEquals(3.0, [[interp.globals objectForName:@"foo"] doubleValue]);
+    [self eval:@"var foo=1 + 2;"];
+    TDEquals(3.0, [self doubleForName:@"foo"]);
 }
 
 - (void)test1Plus2 {
-    NSString *input = @"var foo=1+2;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDEquals(3.0, [[interp.globals objectForName:@"foo"] doubleValue]);
+    [self eval:@"var foo=1+2;"];
+    TDEquals(3.0, [self doubleForName:@"foo"]);
 }
 
 - (void)test1SpaceMinusSpace1 {
-    NSString *input = @"var foo=1 - 1;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDEquals(0.0, [[interp.globals objectForName:@"foo"] doubleValue]);
+    [self eval:@"var foo=1 - 1;"];
+    TDEquals(0.0, [self doubleForName:@"foo"]);
 }
 
 - (void)testTruePlus1 {
-    NSString *input = @"var foo=true + 1;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDEquals(2.0, [[interp.globals objectForName:@"foo"] doubleValue]);
+    [self eval:@"var foo=true + 1;"];
+    TDEquals(2.0, [self doubleForName:@"foo"]);
 }
 
 - (void)testFalsePlus1 {
-    NSString *input = @"var foo=false + 1;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDEquals(1.0, [[interp.globals objectForName:@"foo"] doubleValue]);
+    [self eval:@"var foo=false + 1;"];
+    TDEquals(1.0, [self doubleForName:@"foo"]);
 }
 
 - (void)test2Times2 {
-    NSString *input = @"var foo=2*2;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDEquals(4.0, [[interp.globals objectForName:@"foo"] doubleValue]);
+    [self eval:@"var foo=2*2;"];
+    TDEquals(4.0, [self doubleForName:@"foo"]);
 }
 
 - (void)test2Div2 {
-    NSString *input = @"var foo=2/2;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDEquals(1.0, [[interp.globals objectForName:@"foo"] doubleValue]);
+    [self eval:@"var foo=2/2;"];
+    TDEquals(1.0, [self doubleForName:@"foo"]);
 }
 
 - (void)test3Plus2Times2 {
-    NSString *input = @"var foo=3+2*2;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDEquals(7.0, [[interp.globals objectForName:@"foo"] doubleValue]);
+    [self eval:@"var foo=3+2*2;"];
+    TDEquals(7.0, [self doubleForName:@"foo"]);
 }
 
 - (void)testOpen3Plus2CloseTimes2 {
-    NSString *input = @"var foo=(3+2)*2;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDEquals(10.0, [[interp.globals objectForName:@"foo"] doubleValue]);
+    [self eval:@"var foo=(3+2)*2;"];
+    TDEquals(10.0, [self doubleForName:@"foo"]);
 }
 
 - (void)testNeg2Mod2 {
-    NSString *input = @"var foo=-2%2;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDEquals(-0.0, [[interp.globals objectForName:@"foo"] doubleValue]);
+    [self eval:@"var foo=-2%2;"];
+    TDEquals(-0.0, [self doubleForName:@"foo"]);
 }
 
 - (void)testNeg1Mod2 {
-    NSString *input = @"var foo=-1%2;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDEquals(-1.0, [[interp.globals objectForName:@"foo"] doubleValue]);
+    [self eval:@"var foo=-1%2;"];
+    TDEquals(-1.0, [self doubleForName:@"foo"]);
 }
 
 - (void)test0Mod2 {
-    NSString *input = @"var foo=0%2;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDEquals(0.0, [[interp.globals objectForName:@"foo"] doubleValue]);
+    [self eval:@"var foo=0%2;"];
+    TDEquals(0.0, [self doubleForName:@"foo"]);
 }
 
 - (void)test1Mod2 {
-    NSString *input = @"var foo=1%2;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDEquals(1.0, [[interp.globals objectForName:@"foo"] doubleValue]);
+    [self eval:@"var foo=1%2;"];
+    TDEquals(1.0, [self doubleForName:@"foo"]);
 }
 
 - (void)test2Mod2 {
-    NSString *input = @"var foo=2%2;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDEquals(0.0, [[interp.globals objectForName:@"foo"] doubleValue]);
+    [self eval:@"var foo=2%2;"];
+    TDEquals(0.0, [self doubleForName:@"foo"]);
 }
 
 - (void)test3Mod2 {
-    NSString *input = @"var foo=3%2;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDEquals(1.0, [[interp.globals objectForName:@"foo"] doubleValue]);
+    [self eval:@"var foo=3%2;"];
+    TDEquals(1.0, [self doubleForName:@"foo"]);
 }
 
 - (void)test4Mod2 {
-    NSString *input = @"var foo=4%2;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDEquals(0.0, [[interp.globals objectForName:@"foo"] doubleValue]);
+    [self eval:@"var foo=4%2;"];
+    TDEquals(0.0, [self doubleForName:@"foo"]);
 }
 
 - (void)testMinus1 {
-    NSString *input = @"var foo=-1;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDEquals(-1.0, [[interp.globals objectForName:@"foo"] doubleValue]);
+    [self eval:@"var foo=-1;"];
+    TDEquals(-1.0, [self doubleForName:@"foo"]);
 }
 
 - (void)testMinusMinus1 {
-    NSString *input = @"var foo=--1;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDEquals(1.0, [[interp.globals objectForName:@"foo"] doubleValue]);
+    [self eval:@"var foo=--1;"];
+    TDEquals(1.0, [self doubleForName:@"foo"]);
 }
 
 - (void)test1PlusMinusMinus1 {
-    NSString *input = @"var foo=1 + --1;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDEquals(2.0, [[interp.globals objectForName:@"foo"] doubleValue]);
+    [self eval:@"var foo=1 + --1;"];
+    TDEquals(2.0, [self doubleForName:@"foo"]);
 }
 
 - (void)test1MinusMinusMinus1 {
-    NSString *input = @"var foo=1 - --1;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDEquals(0.0, [[interp.globals objectForName:@"foo"] doubleValue]);
+    [self eval:@"var foo=1 - --1;"];
+    TDEquals(0.0, [self doubleForName:@"foo"]);
 }
 
 - (void)testMinusMinus1Plus1 {
-    NSString *input = @"var foo=--1 + 1;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDEquals(2.0, [[interp.globals objectForName:@"foo"] doubleValue]);
+    [self eval:@"var foo=--1 + 1;"];
+    TDEquals(2.0, [self doubleForName:@"foo"]);
 }
 
 - (void)testMinusMinus1Minus1 {
-    NSString *input = @"var foo=--1 - 1;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDEquals(0.0, [[interp.globals objectForName:@"foo"] doubleValue]);
+    [self eval:@"var foo=--1 - 1;"];
+    TDEquals(0.0, [self doubleForName:@"foo"]);
 }
 
 - (void)testMinusMinusMinus1 {
-    NSString *input = @"var foo=---1;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDEquals(-1.0, [[interp.globals objectForName:@"foo"] doubleValue]);
+    [self eval:@"var foo=---1;"];
+    TDEquals(-1.0, [self doubleForName:@"foo"]);
 }
 
 - (void)testMinusMinusMinusMinus4 {
-    NSString *input = @"var foo=----4;";
-    
-    XPInterpreter *interp = [[[XPInterpreter alloc] init] autorelease];
-    
-    NSError *err = nil;
-    [interp interpretString:input error:&err];
-    TDNil(err);
-    TDEquals(4.0, [[interp.globals objectForName:@"foo"] doubleValue]);
+    [self eval:@"var foo=----4;"];
+    TDEquals(4.0, [self doubleForName:@"foo"]);
 }
 
 @end
