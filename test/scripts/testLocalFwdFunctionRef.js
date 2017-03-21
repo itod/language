7d52457253509399d01de3a2695e40894a60fc4e
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2017-03-21 16:17:07 -0500

    * tests

---
+++ test/scripts/testLocalFwdFunctionRef.js
@@ -0,0 +1,8 @@
+var x = 0
+{
+    var baz = bar
+    baz()
+    sub bar() {
+        x = 3;
+    }
+}
---
+++ test/scripts/testNestedFwdFunctionRef.js
@@ -0,0 +1,9 @@
+var x = 0
+foo()
+sub foo() {
+    var baz = bar;
+    baz()
+    sub bar() {
+        x = 3.0;
+    }
+}
