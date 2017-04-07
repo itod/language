7ce967973a3917227c445b67d6d18e9d7e3e3960
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2017-04-07 18:41:10 -0500

    * test cu

--- test/FNNumberTests.m
+++ test/FNNumberTests.m
@@ -24,74 +24,95 @@
     [super tearDown];
 }
 
-- (void)testMax {
+- (void)testMax1 {
     [self eval:@"var x=max(1);"];
     TDEquals(1.0, [self doubleForName:@"x"]);
-    
+}
+
+- (void)testMax2 {
     [self eval:@"var x=max(1,2);"];
     TDEquals(2.0, [self doubleForName:@"x"]);
-    
+}
+
+- (void)testMax3 {
     [self eval:@"var x=max(1,-2);"];
     TDEquals(1.0, [self doubleForName:@"x"]);
-    
+}
+
+- (void)testMax4 {
     [self eval:@"var x=max(-1,-2);"];
     TDEquals(-1.0, [self doubleForName:@"x"]);
-    
+}
+
+- (void)testMax5 {
     [self eval:@"var x=max('0','2');"];
     TDEquals(2.0, [self doubleForName:@"x"]);
-    
+}
+
+- (void)testMax6 {
     [self eval:@"var x=max([1,2]);"];
     TDEquals(2.0, [self doubleForName:@"x"]);
 }
 
-- (void)testMin {
+- (void)testMin1 {
     [self eval:@"var x=min(1);"];
     TDEquals(1.0, [self doubleForName:@"x"]);
-    
+}
+
+- (void)testMin2 {
     [self eval:@"var x=min(1,2);"];
     TDEquals(1.0, [self doubleForName:@"x"]);
-    
+}
+
+- (void)testMin3 {
     [self eval:@"var x=min(1,-2);"];
     TDEquals(-2.0, [self doubleForName:@"x"]);
-    
+}
+
+- (void)testMin4 {
     [self eval:@"var x=min('0','2');"];
     TDEquals(0.0, [self doubleForName:@"x"]);
-    
+}
+
+- (void)testMin5 {
     [self eval:@"var x=min([1,2]);"];
     TDEquals(1.0, [self doubleForName:@"x"]);
 }
 
-- (void)testRand {
-    {
-        [self eval:@"var x=random(1);"];
-        double d = [self doubleForName:@"x"];
-        TDTrue(d >= 0.0 && d <= 1.0);
-    }
-    {
-        [self eval:@"var x=random(10);"];
-        double d = [self doubleForName:@"x"];
-        TDTrue(d >= 0.0 && d <= 10.0);
-    }
-    {
-        [self eval:@"var x=random(-10);"];
-        double d = [self doubleForName:@"x"];
-        TDTrue(d <= 0.0 && d >= -10.0);
-    }
-    {
-        [self eval:@"var x=random(0,10);"];
-        double d = [self doubleForName:@"x"];
-        TDTrue(d >= 0.0 && d <= 10.0);
-    }
-    {
-        [self eval:@"var x=random(0, -10);"];
-        double d = [self doubleForName:@"x"];
-        TDTrue(d <= 0.0 && d >= -10.0);
-    }
-    {
-        [self eval:@"var x=random(5,12);"];
-        double d = [self doubleForName:@"x"];
-        TDTrue(d >= 5.0 && d <= 12.0);
-    }
+- (void)testRand1 {
+    [self eval:@"var x=random(1);"];
+    double d = [self doubleForName:@"x"];
+    TDTrue(d >= 0.0 && d <= 1.0);
+}
+
+- (void)testRand2 {
+    [self eval:@"var x=random(10);"];
+    double d = [self doubleForName:@"x"];
+    TDTrue(d >= 0.0 && d <= 10.0);
+}
+
+- (void)testRand3 {
+    [self eval:@"var x=random(-10);"];
+    double d = [self doubleForName:@"x"];
+    TDTrue(d <= 0.0 && d >= -10.0);
+}
+
+- (void)testRand4 {
+    [self eval:@"var x=random(0,10);"];
+    double d = [self doubleForName:@"x"];
+    TDTrue(d >= 0.0 && d <= 10.0);
+}
+
+- (void)testRand5 {
+    [self eval:@"var x=random(0, -10);"];
+    double d = [self doubleForName:@"x"];
+    TDTrue(d <= 0.0 && d >= -10.0);
+}
+
+- (void)testRand6 {
+    [self eval:@"var x=random(5,12);"];
+    double d = [self doubleForName:@"x"];
+    TDTrue(d >= 5.0 && d <= 12.0);
 }
 
 @end
