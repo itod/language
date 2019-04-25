23b8c3d42fbc911ececdb979305d0f6738e11b5b
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2019-04-24 20:25:10 -0500

    * repr()

---
+++ src/FNRepr.m
@@ -0,0 +1,40 @@
+//
+//  FNRepr.m
+//  Language
+//
+//  Created by Todd Ditchendorf on 2/14/17.
+//  Copyright © 2017 Celestial Teapot. All rights reserved.
+//
+
+#import "FNRepr.h"
+#import <Language/XPObject.h>
+#import <Language/XPTreeWalker.h>
+#import "XPFunctionSymbol.h"
+#import "XPMemorySpace.h"
+
+@implementation FNRepr
+
++ (NSString *)name {
+    return @"repr";
+}
+
+
+- (XPFunctionSymbol *)symbol {
+    XPFunctionSymbol *funcSym = [XPFunctionSymbol symbolWithName:[[self class] name] enclosingScope:nil];
+    funcSym.nativeBody = self;
+    
+    XPSymbol *obj = [XPSymbol symbolWithName:@"object"];
+    funcSym.orderedParams = [NSMutableArray arrayWithObjects:obj, nil];
+    funcSym.params = [NSMutableDictionary dictionaryWithObjectsAndKeys:
+                      obj, @"object",
+                      nil];
+    
+    return funcSym;
+}
+
+
+- (XPObject *)callWithWalker:(XPTreeWalker *)walker functionSpace:(XPMemorySpace *)space argc:(NSUInteger)argc {
+    return [XPObject string:[[walker.currentSpace objectForName:@"object"] reprValue]];
+}
+
+@end
